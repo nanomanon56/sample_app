45059681debcb5dadcb19e1a3532424f3198dc24
Manon <manon.l.jean@gmail.com>
2017-10-27 19:56:02 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -18,9 +18,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "Help | #{@base_title}"
   end
 
-test "should get about" do
+  test "should get about" do
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
-end
+  end
 end
